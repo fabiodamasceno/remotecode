cf04d3808c5363a6131e3202ce919bd1f9ffe395
Fábio Damasceno <fabio.damasceno@lambda3.com.br>
2019-07-05 07:22:39 -0300

    layout bonito

--- App.js
+++ App.js
@@ -1,24 +1,17 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
+ * Remote Code :D
+ *   https://github.com/fabiodamasceno/remotecode
+
  */
 
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 import codePush from "react-native-code-push";
-console.log(codePush);
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu"
-});
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
+  state = {
+    bicote: false
+  };
   componentDidMount() {
     codePush.sync({
       updateDialog: true,
@@ -26,12 +19,32 @@ export default class App extends Component<Props> {
       checkFrequency: codePush.CheckFrequency.ON_APP_RESUME
     });
   }
+  bicote = () => {
+    this.setState({ bicote: !this.state.bicote });
+  };
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Image
+          style={styles.image}
+          source={{
+            uri: this.state.bicote
+              ? "https://i.ibb.co/LPGM9gM/Guilherme-Porto-Sem-Bigodon.png"
+              : "https://i.ibb.co/LPGM9gM/Guilherme-Porto-Sem-Bigodon.png"
+          }}
+        />
+        <View style={styles.username}>
+          <Text style={styles.name}>Guilherme </Text>
+          <Text style={styles.surname}>Porto</Text>
+        </View>
+        <Text style={styles.instructions}>
+          Você acha que o Guilherme Porto deve voltar a ter bigode?
+        </Text>
+        <TouchableOpacity onPress={this.bicote} style={styles.button}>
+          <Text style={styles.buttonText}>
+            {this.state.bicote ? "Sem Bicote" : "Com Bicote"}
+          </Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -42,16 +55,41 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#F5FCFF"
+    backgroundColor: "#676099",
+    padding: 32
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
+  image: { width: 230, height: 230, borderRadius: 50 },
+  username: {
+    flexDirection: "row",
+    margin: 30,
+    textAlign: "center"
+  },
+  name: {
+    fontWeight: "bold",
+    fontSize: 24,
+    color: "#fff"
+  },
+  surname: {
+    fontSize: 24,
+    color: "#fff"
   },
   instructions: {
+    color: "#fff",
     textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
+    fontSize: 16
+  },
+  button: {
+    width: 300,
+    height: 50,
+    marginTop: 50,
+    backgroundColor: "#f79191",
+    borderRadius: 50,
+    padding: 20,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 20
   }
 });
